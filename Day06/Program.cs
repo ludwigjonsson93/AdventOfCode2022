42caef418ff8a05a7cefc843b98b3e518df3d0aa
Ludwig Jonsson <ludwig.jonsson@capgemini.com>
2022-12-06 16:12:27 +0100

    day 6 begun

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World! Welcome to Day 6 of Advent Of Code");
+Console.WriteLine();
+
+string path = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory) + "/Input.txt";
+string[] lines = File.ReadAllLines(path);
+
+Console.WriteLine("Sum of priorities: " + part1(lines));
+Console.WriteLine();
+//Console.WriteLine("Sum of priorities for the three Elf groups: " + part2(lines));
+
+static int part1(string[] lines)
+{
+    return 0;
+}
