95e4615dec6f0c221ea01f723710c8a33921be2d
Ludwig Jönsson <Ludde_j_@hotmail.com>
2022-12-19 22:11:00 +0100

    Day 19 Project Created

---
+++ Day19/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! Welcome to Day 19 of Advent Of Code");
+Console.WriteLine();
+
+string path = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory) + "/Input.txt";
+string[] lines = File.ReadAllLines(path);
+
+static void part1(string[] lines)
+{
+
+}
