8d6fe085183e96c3f1a41220839ab630ff0b91ce
ludwigjonsson93 <ludwigjonsson93@gmail.com>
2022-12-25 12:49:31 +0100

    Day 25 project created, day 9 solution started

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 Console.WriteLine("Hello, World! Welcome to Day 9 of Advent Of Code");
 Console.WriteLine();
 
@@ -7,5 +10,23 @@ string[] lines = File.ReadAllLines(path);
 
 static void part1(string[] lines)
 {
+    var head = (0, 0);
+    var tail = (0, 0);
+    List<Tuple<int, int>> visited = new List<Tuple<int, int>>();
+    foreach(var line in lines)
+    {
+        string[] instructions = line.Split(' ');
+        switch (instructions[0])
+        {
+            case "U":
 
+                break;
+            case "R":
+                break;
+            case "D":
+                break;
+            case "L":
+                break;
+        }
+    }
 }
